2194a9ab82cdaced1d3341c8f23c1690a52e02ae
AbuBakkarSiddique007 <abubakkar.cce25.iiuc@gmail.com>
2024-11-03 12:26:52 +0600

    17.1---17.6

---
+++ Introduction/one.js
@@ -0,0 +1,91 @@
+// 17-1 Introduction to JavaScript
+
+//Type "log" for console.log() suggestion.
+
+//Run the program:
+//In Terminal: node fileLocation
+
+console.log(1212);
+//1212
+
+console.log("Hello World");
+//Hello World
+
+
+
+// 17-2 Maths Needed to start learning Programming Language
+
+/**
+ *************************************************************
+ *              MATH NEEDED FOR PROGRAMMING
+ * ************************************************************
+ * 1. Number: +ve, -ve, 0 [ 50, -9, 0]
+ * 2. integer: 2, 88, [odd, even]: 2, 4, 897 
+ * 3. float: 5.6, 2.2
+ * 4. Operation: +, -, *, /
+ * 5. More operation: %, [ ]
+ * 6. percentage: 5%, 50 taka er 10% koto?
+ * 7. Sum: 14, 18, 16
+ * 8. Avg: 14, 18, 16
+ * 9. Max/Min: 14, 18, 16
+ * 10. Series: 
+ * 11. Area: Triangle 
+ * 12.  
+*/
+
+
+// 17-3 What is a variable, five things you need to declare a variable
+
+// Don't use var
+
+var age = 24;
+var weight = 52;
+var year = 2024;
+var price = 200;
+
+var money;
+money = 500;
+
+console.log("Age:", age);        //Age: 24
+console.log('Weight: ', weight); //Weight:  52
+console.log("Year: ", year);     //Year:  2024
+console.log("Price: ", price);   //Price:  200
+
+console.log("Money: ", money);   //Money:  500
+
+
+
+
+
+// 17-4 Variable Animated Video
+// 17-5 Variable data types, Numeric, String, Boolean
+
+
+// Numeric:
+var value = 100;
+console.log("The value is : ", value); //The value is :  100
+
+//typeof => check the variable type 
+console.log(typeof value);              //number
+
+// String:
+var name = "Abu Bakkar Siddique";
+var address = "Chittagong";
+var instituteName = 'IIUC';
+
+console.log("My Name is : ", name); //My Name is :  Abu Bakkar Siddique
+console.log(`My self ${name}`);     //My self Abu Bakkar Siddique
+
+console.log(address);               //Chittagong
+console.log(typeof address);        //string
+
+
+// boolean:
+var isLogged = true;
+var pass = false;
+console.log(isLogged);              //true
+console.log(typeof isLogged);       //boolean
+
+
+// 17-6 JavaScript Keyword, Variable name naming convention
+
