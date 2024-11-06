f3d61fe8c860eedd9ec4a2b809755e6413aa72e0
AbuBakkarSiddique007 <abubakkar.cce25.iiuc@gmail.com>
2024-11-07 01:35:02 +0600

    a

--- 04.Conceptual-1/03.NaN.js
+++ 04.Conceptual-1/03.NaN.js
@@ -1,5 +1,5 @@
 var x = 10;
-console.log(typeof x);
+// console.log(typeof x);
 
 
 // isNaN function:
@@ -10,3 +10,19 @@ console.log(typeof x);
 
 // If the value is not a number or cannot be converted into a number, it returns: true
 // Otherwise, it returns: false
+
+var i = 101.111
+console.log(isNaN(i));
+//False, Cause its a number.
+
+var j = "101.111"
+console.log(isNaN(j));
+//False. Cause it can possible to convert into number.
+
+var k = "1212jjj"
+console.log(isNaN(k));
+//True.
+//Cause its not a number/ Not possible to convert into number.
+
+
+
---
+++ 04.Conceptual-1/04.operators.js
@@ -0,0 +1,43 @@
+//Assignment Operator(=)
+
+var a = 10
+var b = 12
+
+//Comparison Operator: => return : true or false.
+//(===)
+// (!==)
+//<
+//>
+// >=
+//<=
+
+console.log( a > b);     //false
+console.log( a >= b);    //false
+
+console.log(a < b);      //true
+console.log(a <= b);     //true
+
+console.log(a === b);   //false
+console.log(a != b);    //true
+// console.log();
+
+
+//Difference between === and ==:
+// == => its just check the value are same or not.
+//=== => it check the value same or not with its data type.
+
+// Loose equality (==)
+10 == 10      // true
+10 == '10'    // true (because '10' is converted to 10)
+
+// Strict equality (===)
+10 === 10     // true
+10 === '10'   // false (because '10' is a string and 10 is a number)
+
+
+
+
+
+
+
+
