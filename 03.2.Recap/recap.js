1d9afd4bc3a915499fc5ac00fcc6dfbdb92e6796
AbuBakkarSiddique007 <abubakkar.cce25.iiuc@gmail.com>
2024-11-09 19:42:22 +0600

    a

---
+++ 03.2.Recap/recap.js
@@ -0,0 +1,12 @@
+// const : value will not change
+// let: value can change
+// var: we should not use it
+
+//Variable
+const a = 100
+if (a > 200) {
+
+} else {
+    
+}
+
---
+++ 04.Conceptual-1/05.Conditions.js
@@ -0,0 +1,29 @@
+var isRaining = true
+
+// if (isRaining === true) {
+//     console.log("We will not go!")
+// }
+// else {
+//     console.log("We will go.");
+
+// }
+
+//Ternary:
+// isRaining === true ? console.log("will not go outside.") : console.log("will go to outside.");
+
+var money = 10
+var isRaining = true
+var taxi = 15
+
+if (isRaining === true && money >= taxi) {
+    console.log("I will go to university.");
+
+}
+else {
+    console.log("I will not  go to university.");
+
+}
+
+//Ternary:
+isRaining === true && money >= taxi ? console.log("will go") : console.log("will not go.");
+
