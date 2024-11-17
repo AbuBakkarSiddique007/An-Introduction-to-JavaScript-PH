2f5469b6ae2b6fc17b1e4a9edeb838b10a611af9
AbuBakkarSiddique007 <abubakkar.cce25.iiuc@gmail.com>
2024-11-18 01:38:35 +0600

    a

--- 07.Array-part-1/01.One.js
+++ 07.Array-part-1/01.One.js
@@ -3,3 +3,114 @@
 
 
 //20.5_1 Array Traversal using for and while Loop
+/**
+ * LOOPING Technique
+ * 1. for loop
+ * 2. while loop
+ * 3. do while --> ignore
+ * 4. for of --> array loop korar jonno
+ * 5. for in --> object loop korar jonno
+ * 
+*/
+
+//Using for of loop:
+let friends = ['Abu', 'Bakkar', 'Siddique', 'Riki', 'Ahmed']
+
+for (let friend of friends) {
+    // console.log(friend);
+
+}
+
+//Abu
+// Bakkar
+// Siddique
+// Riki
+// Ahmed
+
+
+//Using for loop:
+for (let i = 0; i < friends.length; i++) {
+    // console.log(friends[i]);
+}
+
+// Abu
+// Bakkar
+// Siddique
+// Riki
+// Ahmed
+
+
+//Reverse array: 
+let re = ''
+for (let i = 0; i < friends.length; i++) {
+    re = friends[i] + re;
+}
+
+// console.log(re);
+//AhmedRikiSiddiqueBakkarAbu
+
+
+//Array reverse using reverse() method:
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// numbers.reverse()
+// console.log(numbers);
+
+// [
+//     10, 9, 8, 7, 6,
+//      5, 4, 3, 2, 1
+//   ]
+
+
+
+//20.5_2 Reversing an array with/without reverse method
+
+
+let a = [1, 2, 3, 4, 5, 6]
+
+//reverse():
+// a.reverse()
+// console.log(a);
+
+
+//unshift in for of:
+//unshift => Insert new element start of the array
+// let empt_a = []
+// for (const aa of a) {
+    // console.log(aa);
+//     empt_a.unshift(aa)
+
+// }
+// console.log(empt_a);
+//[ 6, 5, 4, 3, 2, 1 ]
+
+
+
+//unshift in for loop:
+
+// let empty_arr = []
+// for (let i = 0; i < a.length; i++) {
+
+//     let num = a[i]
+//     empty_arr.unshift(num)
+
+// }
+// console.log(empty_arr);
+//[ 6, 5, 4, 3, 2, 1 ]
+
+
+//reverse side array:
+let temp_arr = []
+for (let i = a.length - 1; i >= 0; i--){
+    let num = a[i]
+    // console.log(num);
+
+    temp_arr.push(num)
+
+    
+}
+// console.log(temp_arr);
+
+
+//20.5_3 Sort an array problems and solution
