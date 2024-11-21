7de4698c43eef02d4de26035be57926c24e33914
AbuBakkarSiddique007 <abubakkar.cce25.iiuc@gmail.com>
2024-11-21 13:18:06 +0600

    a

---
+++ 09.JS-simple-coding-part-1/One.js
@@ -0,0 +1,21 @@
+//Repo link: https://github.com/ProgrammingHero1/js-problems-part1.git
+//Task link:
+
+
+// 22-1 JS Concepts recap and var, let, and const
+//22-2 Unit Convert Inch to Feet, miles to kilometre
+
+//Conversion:
+//inch to feet convert:
+
+function inchToFeet(inch) {
+    let feet = inch / 12
+    return feet
+}
+
+let height = inchToFeet(67).toFixed(2)
+
+console.log('The height in feet  is: ', height);
+
+
+
